f133727ce6cce64ad7a39c6d1f4364f1148919e1
EVWatson <emily.watson@myob.com>
2019-06-13 14:51:29 +1000

    tests for if customer has enough money, and needs change pass

--- src/CashRegister.java
+++ src/CashRegister.java
@@ -1,2 +1,17 @@
 public class CashRegister {
+    public boolean areCustomerFundsSufficient(int custmerFunds, int merchValue) {
+        boolean result = false;
+        if(custmerFunds >= merchValue){
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean doesCustomerRequireChange(int customerFunds, int merchValue) {
+        boolean result = false;
+        if(customerFunds > merchValue){
+            result = true;
+        }
+        return  result;
+    }
 }
--- tests/CashRegisterTest.java
+++ tests/CashRegisterTest.java
@@ -1,27 +1,56 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CashRegisterTest {
 
-    @Test
-    public void customerHasSufficientFundsToPayWhenAmountGivenIsGreaterOrEqualToMerchandiseValue(){
+    CashRegister cashRegister;
 
+    @Before
+    public void setUp(){
+        this.cashRegister = new CashRegister();
     }
 
     @Test
-    public void whenFundsGivenAreEqualToMerchandiseValueCustomerWillNotReceiveAnyChange(){
+    public void customerHasSufficientFundsToPayWhenAmountGivenIsGreaterOrEqualToMerchandiseValue(){
+        int merchValue = 100;
+        int custmerFunds = 110;
 
+        boolean result = cashRegister.areCustomerFundsSufficient(custmerFunds, merchValue);
+
+        assertTrue(result);
     }
 
     @Test
     public void whenFundsGivenAreLessThanMerchandiseValueCustomerIsUnableProceedWithTransaction(){
+        int merchValue = 100;
+        int customerFunds = 99;
 
+        boolean result = cashRegister.areCustomerFundsSufficient(customerFunds, merchValue);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void whenFundsGivenAreEqualToMerchandiseValueCustomerWillNotReceiveAnyChange(){
+        int merchValue = 100;
+        int customerFunds = 100;
+
+        boolean result = cashRegister.doesCustomerRequireChange(customerFunds, merchValue);
+
+        assertFalse(result);
     }
 
     @Test
     public void whenFundsGivenAreGreaterThanMerchandiseValueTheCustomerWillReceiveChange(){
-        
+        int merchValue = 100;
+        int customerFunds = 110;
+
+        boolean result = cashRegister.doesCustomerRequireChange(customerFunds, merchValue);
+
+        assertTrue(result);
+
     }
 
 
