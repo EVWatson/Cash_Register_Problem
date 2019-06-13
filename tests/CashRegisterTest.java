0242696b9df7e7cec1ba60eaac406a49977fbfd7
EVWatson <emily.watson@myob.com>
2019-06-13 14:36:06 +1000

    started listing tests for cashRegister

---
+++ tests/CashRegisterTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashRegisterTest {
+
+    @Test
+    public void customerHasSufficientFundsToPayWhenAmountGivenIsGreaterOrEqualToMerchandiseValue(){
+
+    }
+
+    @Test
+    public void whenFundsGivenAreEqualToMerchandiseValueCustomerWillNotReceiveAnyChange(){
+
+    }
+
+    @Test
+    public void whenFundsGivenAreLessThanMerchandiseValueCustomerIsUnableProceedWithTransaction(){
+
+    }
+
+    @Test
+    public void whenFundsGivenAreGreaterThanMerchandiseValueTheCustomerWillReceiveChange(){
+        
+    }
+
+
+}
